37a37a7fb62c5ccd9815d6956145d5d82ceaceaa
caiminiu09190 <930216136@qq.com>
2019-04-15 14:12:56 +0800

    drag 拖拽card

--- components/drag/DragContainer.tsx
+++ components/drag/DragContainer.tsx
@@ -1,56 +1,15 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import {withDragDropContext} from './withDragDropContext';
-import Store from './store';
-
+import WithDragDropContext from '../withdrag-dropcontext';
 export interface DragContainerProps {
   children: React.ReactNode;
-  dataSource: object;
-  type: string;
-  dragType?: 'move'|'copy';
-  getStore: (store: Store) => void;
-  onChange?: (values:any) => any;
 }
-
-@withDragDropContext
-export default class DragDropContainer extends React.Component<DragContainerProps> {
-  static defaultProps = {
-    dragType: 'move',
-  }
-  static childContextTypes = {
-    store: PropTypes.object,
-    type: PropTypes.string,
-  };
-  store: Store;
-  constructor(props: DragContainerProps,context: any) {
-    super(props,context);
-    this.state = {
-
-    }
-    this.store = new Store(props.dataSource, props.dragType);
-    if (props.getStore) {
-      props.getStore(this.store)
-    }
-    this.store.subscribe(() => {
-      props.onChange && props.onChange(this.store.get())
-    })
-  }
-  componentWillReceiveProps(props:DragContainerProps) {
-    if (props.dataSource !== this.props.dataSource) {
-      this.store.reset(props.dataSource)
-    }
-  }
-
-  getChildContext() {
-    return {store:this.store,type: this.props.type};
-  }
-
+class DragDropContainer extends React.Component<DragContainerProps> {
   render() {
     return (
       <div>
         {this.props.children}
-        </div>
+      </div>
     )
   }
 }
-
+export default WithDragDropContext(DragDropContainer)
